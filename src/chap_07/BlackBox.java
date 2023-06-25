323179fd24a8d5b562845dad1ff4c945a062a51b
jjindol <andy15948@naver.com>
2023-06-25 22:10:44 +0900

    second commit

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -1,7 +1,12 @@
 package chap_07;
 
-public class BlackBox {
-    public static void main(String[] args) {
+public class BlackBox { // 설계도
+    String modelName;
+    String resolution;
+    int price;
+    String color;
+
+    static boolean canAutoReport = false; // 자동 신고 기능, 클래스 변수(클래스 내부에 선언됨)
 
     }
-}
+
