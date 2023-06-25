951a72d5a913ce1ac9bfd3d95f777cee18c4712d
jjindol <andy15948@naver.com>
2023-06-25 22:57:49 +0900

    second commit

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -8,5 +8,27 @@ public class BlackBox { // 설계도
 
     static boolean canAutoReport = false; // 자동 신고 기능, 클래스 변수(클래스 내부에 선언됨)
 
+    void autoReport() { // 전달값, 반환값 x
+        if(canAutoReport) {
+            System.out.println("충돌이 감지되어 자동으로 신고합니다.");
+        } else {
+            System.out.println("자동 신고 기능이 지원되지 않습니다.");
+        }
     }
+
+    void insertMemoryCard(int capacity) { // 전달값 존재
+        System.out.println("메모리 카드가 삽입되었습니다.");
+        System.out.println("용량은 " + capacity + "GB 입니다.");
+    }
+
+    int getVideoFileCount(int type) { // 전달값과 반환값 존재
+        if(type==1) {
+            return 9;
+        } else if(type==2) {
+            return 1;
+        }
+        return 10;
+    }
+
+}
 
---
+++ src/chap_07/_04_Method.java
@@ -0,0 +1,21 @@
+package chap_07;
+
+public class _04_Method {
+    public static void main(String[] args) {
+        BlackBox b1 = new BlackBox();
+        b1.modelName = "BMW";
+
+        b1.autoReport(); // 지원 안됨
+        BlackBox.canAutoReport = true;
+        b1.autoReport(); // 지원 됨
+
+        b1.insertMemoryCard(256);
+
+        // 일반 영상: 1(type)
+        // 충돌 감지 영상: 2
+
+        System.out.println("일반 영상 파일 수: " + b1.getVideoFileCount(1));
+        System.out.println("출동 감지 영상 파일 수: " + b1.getVideoFileCount(2));
+
+    }
+}
