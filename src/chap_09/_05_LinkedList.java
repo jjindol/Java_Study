6abe3979e5d70cf920fb5edca3b5cc978a78f611
jjindol <andy15948@naver.com>
2023-07-01 15:13:01 +0900

    230701 commit

--- src/chap_09/_05_LinkedList.java
+++ src/chap_09/_05_LinkedList.java
@@ -1,5 +1,6 @@
 package chap_09;
 
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class _05_LinkedList {
@@ -27,6 +28,47 @@ public class _05_LinkedList {
         list.add(1, "teacher");
         System.out.println("추가 후: " + list.get(1));
 
+        System.out.println("-----------------");
+
         // 삭제
+        System.out.println("삭제 전 학생 수: " + list.size());
+        list.remove(list.size() - 1);
+        list.removeFirst();
+        list.removeLast();
+        System.out.println("삭제 후 학생 수: " + list.size());
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println("-----------------");
+
+        // 변경
+        list.set(0, "student0");
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println("-----------------");
+
+        // 전체 삭제
+        list.clear();
+        if(list.isEmpty()) {
+            System.out.println("학생 수: " + list.size());
+            System.out.println("리스트가 비었습니다");
+        }
+        System.out.println("-----------------");
+
+        list.add("student4");
+        list.add("student2");
+        list.add("student1");
+        list.add("student3");
+
+
+        Collections.sort(list);
+
+        for (String s :
+                list) {
+            System.out.println(s);
+        }
+
+
     }
 }
