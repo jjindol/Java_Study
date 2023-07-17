d78faeed28fdfda701b235324ec6ac29d303a18c
jjindol <andy15948@naver.com>
2023-07-17 18:26:07 +0900

    2307017 commit

---
+++ src/chap_11/_04_Finally.java
@@ -0,0 +1,27 @@
+package chap_11;
+
+public class _04_Finally {
+    public static void main(String[] args) {
+        try {
+            System.out.println("택시의 문을 연다.");
+            throw new Exception("휴무 택시");
+        } catch (Exception e) {
+            System.out.println("! 문제 발생: " + e.getMessage());
+        } finally {
+            System.out.println("택시의 문을 닫는다."); // 무조건 실행되는 문장
+        }
+        System.out.println("------------------");
+
+        try {
+            System.out.println(3 / 0);
+        } finally {
+            System.out.println("프로그램 정상 종료");
+        }
+
+
+    }
+
+}
+// try + catch
+// try + catch + finally
+// try + finally
