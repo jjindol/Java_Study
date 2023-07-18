25b1433b3cb82599fc2a2df093334514ab93c571
jjindol <andy15948@naver.com>
2023-07-18 13:33:36 +0900

    2307018 commit

--- src/chap_11/TryWithResources.java
+++ src/chap_11/TryWithResources.java
@@ -15,10 +15,28 @@ class MyFileWriter implements AutoCloseable {
 }
 
 
-
-
 public class TryWithResources {
     public static void main(String[] args) {
+        MyFileWriter writer1 = null;
+        try {
+            writer1 = new MyFileWriter();
+            writer1.write("자바 공부.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer1.close(); // close 실행하다가 문제가 발생할 수 있어 그에 대한 처리를 해 줌
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
 
+        System.out.println("---------------------");
+
+        try(MyFileWriter writer2 = new MyFileWriter()) {
+            writer2.write("파이썬 공부");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
