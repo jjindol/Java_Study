9b3afa30c331e0f4fbf14db9ce6406c81a4cedf6
jjindol <andy15948@naver.com>
2023-08-27 21:26:28 +0900

    230827 commit

--- src/chap_13/_02_Output.java
+++ src/chap_13/_02_Output.java
@@ -12,5 +12,36 @@ public class _02_Output {
         System.out.printf("%+6d%n", 1234); // 6자리 확보 후 + 부터 시작
         System.out.printf("%,+15d%n", 1000000000);
         System.out.printf("%-6d%n", 1234); // 왼쪽 정렬
+
+        System.out.println("--------실수-------");
+        System.out.printf("%f%n", Math.PI);
+        System.out.printf("%.2f%n", Math.PI); // 소수점 둘째 자리까지 표현
+        System.out.printf("%6.2f%n", Math.PI); // 6자리 공간 확보 후 출력
+        System.out.printf("%-6.2f%n", Math.PI); // 왼쪽 정렬
+        System.out.printf("%06.2f%n", Math.PI); // 왼쪽 정렬
+
+        System.out.println("--------문자열-------");
+        System.out.printf("%s%n", "Java");
+        System.out.printf("%6s%n", "Java"); // __Java
+        System.out.printf("%-6s%n", "Java"); // 좌측 정렬
+        System.out.printf("%6.2s%n", "Java"); // ____Ja
+        System.out.printf("%-6.2s%n", "Java"); // ____Ja
+
+        System.out.println("--------응용1-------");
+        System.out.println("이름 영어 수학 평균");
+        System.out.println("학생1 " + 90 + " " + 80 + " " + 85.0);
+        System.out.println("학생2 " + 100 + " " + 100 + " " + 100.0);
+        System.out.println("학생3 " + 95 + " " + 100 + " " + 97.5);
+
+        System.out.println("--------응용2-------");
+        System.out.println("이름      영어   수학   평균"); // 공백: 6 3 3
+        System.out.printf("%-6s %4d %4d %6.1f%n", "학생1", 90, 80, 85.0);
+        System.out.printf("%-6s %4d %4d %6.1f%n", "학생2", 100, 100, 100.0);
+        System.out.printf("%-6s %4d %4d %6.1f%n", "학생3", 95, 100, 97.5);
+
     }
 }
+
+
+// %d: decimal integer: 10진 정수 형식
+// %f: floating-point: 부동 소수점 형식
